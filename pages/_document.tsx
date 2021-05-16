be917ae8134668d435ce3ac641872fed2e57ea06
Tiago Schmidt <tiagohammett@gmail.com>
2021-05-16 11:39:44 -0300

    fix: adds key to fonts preload mapping

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Fragment } from 'react'
 
 const fonts = ['Rubik-Bold', 'Rubik-Regular'];
 
@@ -8,10 +9,10 @@ export default class MyDocument extends Document {
       <Html lang="en-US">
         <Head>
           {fonts.map(fontName => (
-            <>
+            <Fragment key={`font-preload-${fontName}`}>
               <link rel="preload" href={`/fonts/${fontName}.woff2`} as="font" type="font/woff2" crossOrigin="anonymous"/>
               <link rel="preload" href={`/fonts/${fontName}.woff`} as="font" type="font/woff" crossOrigin="anonymous"/>
-            </>
+            </Fragment>
           ))}
         </Head>
         <body>
