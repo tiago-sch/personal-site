628057a5599942848243969c60abcf313573f549
Tiago Schmidt <tiagohammett@gmail.com>
2021-07-15 11:08:52 -0300

    chore: adds LD JSON person microdata

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,27 @@ import TechTalk from '../components/TechTalk'
 import PastProjects from '../components/PastProjects'
 import BeyondDev from '../components/BeyondDev'
 
+const LDJson = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "email": "tiagohammett@gmail.com",
+  "image": "https://www.tiagoschmidt.com/images/profile-photo.png",
+  "jobTitle": "Software Developer",
+  "name": "Tiago Schmidt",
+  "birthPlace": "Porto Alegre, RS - Brazil",
+  "birthDate": "1990-09-15",
+  "gender": "male",
+  "nationality": "Brazilian",
+  "url": "https://www.tiagoschmidt.com",
+  "sameAs" : [
+    "https://www.facebook.com/schmidt.tiago/",
+    "https://www.linkedin.com/in/t-schmidt/",
+    "https://github.com/schmidtas/",
+    "https://www.instagram.com/tiago.sch/",
+    "https://www.youtube.com/channel/UC3rwmHd3_QSePebtxolkTng"
+  ]
+}
+
 export default function Home() {
   if (process.browser) {
     console.info('Interested in the code? Let me know and I can send you the git repo or maybe schedule a quick call!! 😄')
@@ -26,6 +47,11 @@ export default function Home() {
       <TechTalk />
       <PastProjects />
       <BeyondDev />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(LDJson) }}
+      />
     </>
   )
 }
