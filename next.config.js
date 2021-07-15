08ee7ed058f56148ed97b3554ceaf39b8e4119f2
Tiago Schmidt <tiagohammett@gmail.com>
2021-07-15 10:11:26 -0300

    chore: adds next config with security params

---
+++ next.config.js
@@ -0,0 +1,34 @@
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'"
+  }
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
