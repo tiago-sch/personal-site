6bd6318a749393c9d54144355eeb5572836ddd58
Tiago Schmidt <tiagohammett@gmail.com>
2021-07-15 10:24:11 -0300

    chore: adds favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,11 @@ export default class MyDocument extends Document {
               <link rel="preload" href={`/fonts/${fontName}.woff`} as="font" type="font/woff" crossOrigin="anonymous"/>
             </Fragment>
           ))}
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <meta name="msapplication-TileColor" content="#6d23b6" />
+          <meta name="theme-color" content="#6d23b6" />
         </Head>
         <body>
           <Main />
