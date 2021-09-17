e6d6c093b71b52d2a84c5b65ff5f3e5768d825af
Tiago Schmidt <tiagohammett@gmail.com>
2021-09-17 10:00:51 -0300

    feat: update to next 11 and dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=5"
         />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       </Head>
